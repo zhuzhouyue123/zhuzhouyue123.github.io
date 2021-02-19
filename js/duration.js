05285e317c58646a23fc14270bd0d9e0733ee9ac
zhuzhouyue123 <zhuzhouyue2005@outlook.com>
2021-02-19 12:13:57 +0800

    Site updated: 2021-02-19 12:13:56

---
+++ js/duration.js
@@ -0,0 +1,27 @@
+!(function() {
+  function update() {
+    var now = new Date();
+    var grt = new Date("2021-02-16 19:40:32");  /** 此处是计时的起始时间 **/
+    now.setTime(now.getTime()+250);
+    days = (now - grt ) / 1000 / 60 / 60 / 24;
+    dnum = Math.floor(days);
+    hours = (now - grt ) / 1000 / 60 / 60 - (24 * dnum);
+    hnum = Math.floor(hours);
+    if(String(hnum).length === 1 ){
+      hnum = "0" + hnum;
+    }
+    minutes = (now - grt ) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+    mnum = Math.floor(minutes);
+    if(String(mnum).length === 1 ){
+      mnum = "0" + mnum;
+    }
+    seconds = (now - grt ) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+    snum = Math.round(seconds);
+    if(String(snum).length === 1 ){
+      snum = "0" + snum;
+    }
+    document.getElementById("timeDate").innerHTML = "网站来到人间已经&nbsp"+dnum+"&nbsp天";
+    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+  }
+  setInterval(update, 1000);
+})();
