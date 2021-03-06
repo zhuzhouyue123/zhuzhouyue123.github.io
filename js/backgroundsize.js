7802d94a7abecb3bd11b0f32c3fef57fe3f50bd7
zhuzhouyue123 <zhuzhouyue2005@outlook.com>
2021-03-06 23:35:02 +0800

    Site updated: 2021-03-06 23:35:02

--- js/backgroundsize.js
+++
@@ -1,9 +0,0 @@
-const bannerContainer = $("#banner");
-const viewBg = $("#web_bg");
-const bannerMask = $("#banner .mask");
-const bg = $(bannerContainer).css("background-image")
-$(viewBg).css("background-image", bg);
-$(bannerContainer).css("background-image");
-const color = #(bannerMask).css("background-color");
-$(bannerMask).css("background-color", `rgba(0,0,0,0)`);
-$(viewBg).css("background-color", color);
